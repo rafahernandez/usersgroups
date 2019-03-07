528469b0aaa5ee14ed075c715b39669916535a38
Rafael  Hernandez <joraheta@gmail.com>
2019-03-06 20:03:01 -0600

    Remove fields from json

--- app/Group.php
+++ app/Group.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at', 'updated_at', 'pivot'
+    ];
     public function users()
     {
         return $this->belongsToMany('App\User', 'group_user', 'group_id', 'user_id');
